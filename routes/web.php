092f0c1655a2adf3b58d1a55d2df4bb3c40d3381
Denilson de Oliveira Carneiro <86687247+Denils0n@users.noreply.github.com>
2024-09-08 21:27:13 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,5 +18,6 @@ Route::resource('books', BookController::class);
 Route::resource('authors', AuthorController::class);
 Route::resource('categories', CategoryController::class);
 Route::resource('publishers', PublisherController::class);
+Route::resource('users', PublisherController::class);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
